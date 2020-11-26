53245db08792871444ababab290d533eff91a611
pastak <pasta0915@gmail.com>
2020-11-26 23:29:18 +0900

    feat: nested list
    fix: https://github.com/pastak/scrapbox-converter/issues/51

--- packages/md2sb/test/simpleReteral.ts
+++ packages/md2sb/test/simpleReteral.ts
@@ -1,8 +1,6 @@
 import test from 'ava';
 import loadAndAssert from './helpers/loadAndAssert';
 
-// ['blockquote', 'codeblock', 'heading', 'hr', 'list', 'paragraph', 'table']
-// 'list' is skiped: ref: https://github.com/wooorm/remark/issues/222
-['blockquote', 'codeblock', 'heading', 'hr', 'paragraph', 'table'].forEach((type) => {
+['blockquote', 'codeblock', 'heading', 'hr', 'list', 'paragraph', 'table'].forEach((type) => {
   test('convert ' + type, (t) => loadAndAssert(t, type));
 });
